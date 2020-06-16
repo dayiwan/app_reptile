499650b36a78ab6b846d6ed047a4e45297803da1
evan <evan@evandeMacBook-Pro.local>
2020-06-16 12:51:06 +0800

    tianyanxinwenapp

--- tianyanxinwen_app/tianyanxinwen_app/items.py
+++ tianyanxinwen_app/tianyanxinwen_app/items.py
@@ -9,6 +9,9 @@ import scrapy
 
 
 class TianyanxinwenAppItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    author = scrapy.Field()
+    category = scrapy.Field()
+    time = scrapy.Field()
+    img_list = scrapy.Field()
+    content = scrapy.Field()
--- tianyanxinwen_app/tianyanxinwen_app/spiders/master.py
+++ tianyanxinwen_app/tianyanxinwen_app/spiders/master.py
@@ -2,20 +2,16 @@
 import scrapy
 import json
 import time
+from urllib.parse import urljoin
 from scrapy import Request
 from urllib.parse import urlencode
+from ..items import TianyanxinwenAppItem
 
 
 class MasterSpider(scrapy.Spider):
     name = 'master'
     start_url = 'http://jgz.app.todayguizhou.com/appAPI/nav-mylist.html?'
     news_list_host = 'http://jgz.app.todayguizhou.com/appAPI/news-getCatePage.html?'
-    # dt = "2019-01-01 00:00:00"
-    # # 转换成时间数组
-    # timeArray = time.strptime(dt, "%Y-%m-%d %H:%M:%S")
-    # # 转换成时间戳
-    # timestamp = int(time.mktime(timeArray))
-    # if_next_page = True
 
     def start_requests(self):
         """获取新闻类别列表"""
@@ -31,47 +27,51 @@ class MasterSpider(scrapy.Spider):
         category_list = json.loads(response.text)['data']['category_list']
         for category in category_list:
             url = self.news_list_host + urlencode({'cate_id': category['cate_id']})
-            yield Request(url=url, callback=self.news_list_parse)
+            cate_name = category['cate_name']
+            yield Request(url=url, callback=self.news_list_parse, meta={'cate_name': cate_name})
             """翻页,只翻前100页"""
-            # self.if_next_page = True
-            # page_num = 2
             for page in range(2, 100):
                 data = {
                     'cate_id': category['cate_id'],
                     'page': str(page)
                 }
                 next_url = self.news_list_host + urlencode(data)
-                yield Request(url=next_url, callback=self.news_list_parse)
-            #     page_num += 1
+                yield Request(url=next_url, callback=self.news_list_parse, meta={'cate_name': cate_name})
 
     def news_list_parse(self, response):
         """获取新闻详情链接"""
         special_host = 'http://jgz.app.todayguizhou.com/appAPI/special-index.html'
         news_list = json.loads(response.text)['data']['list']
         for news in news_list:
-            # print(news['news_addtime'])
+            info_dict = dict()
             if news['is_special'] == 1:     # 判断该新闻是否是专题新闻
                 special_data = {'special_id': news['special_id']}
                 special_url = special_host + urlencode(special_data)
-                yield Request(url=special_url, callback=self.news_list_parse)
+                yield Request(url=special_url, callback=self.news_list_parse, meta={'cate_name': response.meta['cate_name']})
             else:
                 news_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(news['news_addtime'])))
                 news_url = news['shareurl']
-                yield Request(url=news_url, callback=self.news_detail_parse, meta={'time': news_time})
-
-            # if int(news['news_addtime']) < self.timestamp:   # 如果某一条新闻的时间早于2019年一月一日，就不再翻页
-            #     self.if_next_page = False
-            #     break
-            # print(news['news_title'])
-        # print('------------', response.meta['cate_name'], '------------------')
+                info_dict['url'] = news_url
+                info_dict['title'] = news['news_title']
+                info_dict['author'] = news['news_reporterName']
+                info_dict['category'] = response.meta['cate_name']
+                info_dict['time'] = news_time
+                yield Request(url=news_url, callback=self.news_detail_parse, meta=info_dict)
 
     def news_detail_parse(self, response):
         """获取新闻详情"""
-        try:
-            title = response.xpath('//div[@class="detail warpper"]/h1/text()').extract()[0]
-        except:
-            title = response.xpath('//title/text()').extract()[0]
+        item = TianyanxinwenAppItem()
+        host = 'http://jgz.app.todayguizhou.com'
         content = response.xpath('normalize-space(//div[@class="detail-item"])').extract()[0]
-        print('----------', title, '-----------------')
-        print('!!!!!!!!!!', response.meta['time'], '!!!!!!!!!!!!!!!')
-        print(content)
+        img_list = response.xpath('//img/@src').extract()
+        item['img_list'] = list()
+        if len(img_list) > 0:
+            for img in img_list:
+                'http' in img or item['img_list'].append(urljoin(host, img))
+        item['title'] = response.meta['title']
+        item['author'] = response.meta['author']
+        item['category'] = response.meta['category']
+        item['time'] = response.meta['time']
+        item['content'] = content
+        print(item['img_list'])
+
